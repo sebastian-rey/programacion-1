cc2da3d313683e4592641f41bed221d080a7405f
sebastian-rey <sebastiankpo117@gmail.com>
2022-04-22 14:21:03 -0300

    22-4

---
+++ bootstrap5/funciones_flechas/main.js
@@ -0,0 +1,16 @@
+
+
+
+const mostrar = () => 
+
+//Solo si se ejecuta una instruccion no se necesitara las llaves {}
+
+alert ('Probando funcion flecha')
+
+
+const boton = document.getElementById("btn_mostrar")
+
+
+
+
+boton.addEventListener("click", mostrar)
